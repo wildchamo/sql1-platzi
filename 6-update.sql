2035f4d6e20bd8bd0fa73e0ea8f529e3e089aad3
Jose Luis <wildchamo@gmail.com>
2025-08-02 15:39:14 -0500

    Update client phone numbers for individuals named Laura and add a query to retrieve their details.

--- 6-update.sql
+++ 6-update.sql
@@ -8,3 +8,20 @@ update clients
 set phone_number = '+573142758676'
 where email =  'juana73@jacobson.com'
 limit 1;
+
+
+-- todas personas cuyo nombre tenga Laura, le quitas el telefono
+
+update clients
+set phone_number = null
+where name like '%Laura%';
+
+
+-- todas las personas cuyo nombre tenga Laura, le quitas el telefono
+
+update clients
+set phone_number = null
+where name like '%Laura%';
+
+
+select client_id, name, email, phone_number from clients where name like '%Laura%';
