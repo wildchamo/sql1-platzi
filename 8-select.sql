9c5a8673dab8bd97bbdc42e42739be315cb36da9
Jose Luis <wildchamo@gmail.com>
2025-08-02 20:12:57 -0500

    Enhance SQL selection script by adding multiple queries for retrieving product data, including calculations for total value and various LIMIT clauses. Introduce new examples for selecting current time and database name.

--- 8-select.sql
+++ 8-select.sql
@@ -1,4 +1,4 @@
-select columnas, variables
+select columnas, variables, funciones too ! (price* stock)
 from tablas!!!! pueden ser 1 0 mas tablas
 where  importante el where! la condición
 group by
@@ -6,5 +6,29 @@ having
 order by
 limit ;
 
+select now();
+select database();
 
 
+select * from products limit 1;
+
+select * from products limit 1\G;
+
+
+select name, sku,stock,price, product_id from products limit 1;
+
+
+select name, sku,stock,price, product_id, price*stock as total from products where price <=100 and stock > 90 order by stock desc;
+
+
+
+select name, sku,stock,price, product_id, round(price*stock) as total from products where price <=100 and stock > 90 order by total desc;
+
+
+select name, sku,stock,price, product_id, round(price*stock) as total from products order by total asc;
+
+select name, sku,stock,price, product_id, round(price*stock) as total from products order by total desc limit 2,10;
+
+limit a 
+limit 0, a
+limit b, a
