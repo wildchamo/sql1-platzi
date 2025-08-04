6420ba41ced1bb255b06ee2806d5752dfe5a2fe1
Jose Luis <wildchamo@gmail.com>
2025-08-04 11:40:55 -0500

    Add new SQL script for retrieving bill and product information. This includes queries for counting products associated with bills, utilizing left joins for data retrieval, and demonstrating the use of group by for aggregating results. Enhancements improve clarity in data representation and functionality.

---
+++ 12-super-join.sql
@@ -0,0 +1,39 @@
+select b.bill_id, b.status, c.name,
+count(bp.bill_product_id) as number_of_products
+from bills as b
+
+left join clients as c
+on b.client_id=c.client_id
+
+left join bill_products as bp
+on b.bill_id=bp.bill_id
+limit 30;
+
+-- un join debe apuntar siempre a la tabla inicial? naa
+
+select b.bill_id, b.status, c.name,
+count(bp.bill_product_id) as number_of_products
+from bills as b
+
+left join clients as c
+on b.client_id=c.client_id
+
+left join bill_products as bp
+on b.bill_id=bp.bill_id
+group by b.bill_id 
+limit 30;
+
+
+-- group by es para agrupar los resultados por una columna, es decir, si tienes una columna con valores repetidos, group by te permite agruparlos y hacer operaciones sobre ellos.
+
+-- count es para contar el número de filas que hay en una tabla.
+
+-- left join es para unir dos tablas y traer todos los datos de la tabla izquierda y los datos de la tabla derecha que coincidan con la condición.
+
+select count(*) from bill_products where bill_id=2;
+
+
+---
+
+
+ç+
