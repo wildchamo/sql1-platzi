d5c55d8f600433804d4375d76e0e041fb8c4531f
Jose Luis <wildchamo@gmail.com>
2025-08-04 11:21:08 -0500

    Enhance SQL script for joining investment and products tables by adding additional queries to retrieve detailed product information, including stock calculations and status checks. This improves data clarity and expands the functionality of the existing queries.

--- 11-joins.sql
+++ 11-joins.sql
@@ -24,3 +24,16 @@ from invesment as i
 left join products as p
 on i.product_id=p.product_id
 limit 10;
+
+
+-- podrías traer solo stock ya que solo está en una sola tabla!!! el problema es que no te da resultado legible
+
+
+select p.product_id as pid, p.name, p.price, p.stock, i.amount, round(i.amount/p.price) as audited_stock, if(round(i.amount/p.price)=p.stock, 'ok', 'error') as status
+from invesment as i
+left join products as p
+on i.product_id=p.product_id
+limit 10;
+
+
+-- convertir datos en informaciòn! puedes hacer join de N tablas!
